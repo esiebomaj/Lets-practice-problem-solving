8f7b4b57f37fb090fc6393db00b865d3a2b96ebe
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-02 07:25:00 +0100

    Time: 1410 ms (31.30%), Space: 20.2 MB (92.57%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,4 +1,5 @@
 import math
+import heapq
 
 class Pair:
     def __init__(self, distance, coord):
@@ -8,17 +9,37 @@ class Pair:
         return f"{self.distance}|{self.coord}"
         
 class Solution:
+    # my brute force solution using sorting
+    # Time: O(NlogN) 
+#     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+#         for i in range(len(points)): #O(n)
+#             point = points[i]
+#             d = math.sqrt((point[0]*point[0]) + (point[1]*point[1]))
+#             points[i] = Pair(d, point)
+            
+#         points = sorted(points, key=lambda x:x.distance, reverse=True) # O(nlogn)
+        
+#         res = []
+#         for i in range(k): # O(n)
+#             res.append(points.pop().coord)
+        
+#         return res
+
+
+    # Optimized solution using a heap instead of sorting
+    # Time: O(KlogN)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         for i in range(len(points)): #O(n)
             point = points[i]
             d = math.sqrt((point[0]*point[0]) + (point[1]*point[1]))
-            points[i] = Pair(d, point)
+            points[i] = (d, point)
             
-        points = sorted(points, key=lambda x:x.distance, reverse=True) # O(nlogn)
+        heapq.heapify(points) #O(n)
         
         res = []
-        for i in range(k): # O(n)
-            res.append(points.pop().coord)
+        
+        for i in range(k): # O(k)
+            res.append(heapq.heappop(points)[1]) #O(logn)
         
         return res
         
