339b7c1dea75e39fc2b825e3a74ccb6540ddb913
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-09 15:48:30 +0100

    Time: 46 ms (53.15%), Space: 14 MB (11.88%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,27 +1,54 @@
 class Solution:
     
 #     def climbStairs(self, n: int) -> int:
+#         memo = {}
+#         def dfs(n):
+#             if n == 0:
+#                 return 0
+#             if n == 1:
+#                 return 1
+#             if n == 2:
+#                 return 2
+            
+#             if n in memo:
+#                 return memo[n]
+            
+#             rt = self.climbStairs(n-1) + self.climbStairs(n-2)
+            
+#             memo[n] = rt
+#             return rt
+        
+#         return dfs(n)
+            
+    
+#     def climbStairs(self, n: int) -> int:
 #         if n == 1:
 #             return 1
 #         if n == 2:
 #             return 2
         
-#         return self.climbStairs(n-1) + self.climbStairs(n-2)
-    
+#         dp = [0]*n
+#         dp[0] = 1
+#         dp[1] = 2
+        
+#         for i in range(2,n):
+#             dp[i] = dp[i-1] + dp[i-2]
+        
+#         return dp[-1]
+
+
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
-        
-        dp = [0]*n
-        dp[0] = 1
-        dp[1] = 2
-        print(dp)
+
+        a,b = 1,2
+
         for i in range(2,n):
-            dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[-1]
+            a,b = b,a+b
+
+        return b
     
         
         
