3f0545729a9e5b31d936e22f9c54e2259ba39fe1
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-10 18:36:07 +0100

    Time: 3628 ms (12.24%), Space: 14.1 MB (86.22%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,22 +1,45 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        ###### more intuitive solution using M*N time and space
+#         dp = [[0]*(amount+1) for coin in coins]
         
-        dp = [[0]*(amount+1) for coin in coins]
+#         for i in range(len(coins)):
+#             dp[i][0] = 0
         
-        for i in range(len(coins)):
-            dp[i][0] = 0
+#         for i in range(1, amount+1):
+#             dp[0][i] = float(inf) if i%coins[0] != 0 else i//coins[0]
+        
+        
+#         for r in range(1, len(coins)):
+#             for c in range(1, amount+1):
+#                 coin = coins[r]
+#                 if c >= coin:
+#                     dp[r][c] = min(dp[r-1][c], dp[r][c-coin]+1)
+#                 else:
+#                     dp[r][c] = dp[r-1][c]
+                    
+        
+#         return -1 if dp[-1][-1] == float(inf) else dp[-1][-1]
+
+
+
+        ### more Efficient solution using M*N time and M space
+    
+        dp = [0]*(amount+1) 
         
         for i in range(1, amount+1):
-            dp[0][i] = float(inf) if i%coins[0] != 0 else i//coins[0]
-        
+            dp[i] = float(inf) if i%coins[0] != 0 else i//coins[0]
+                    
         
         for r in range(1, len(coins)):
             for c in range(1, amount+1):
                 coin = coins[r]
                 if c >= coin:
-                    dp[r][c] = min(dp[r-1][c], dp[r][c-coin]+1)
-                else:
-                    dp[r][c] = dp[r-1][c]
+                    dp[c] = min(dp[c], dp[c-coin]+1)
                     
         
-        return -1 if dp[-1][-1] == float(inf) else dp[-1][-1]
+        return -1 if dp[-1] == float(inf) else dp[-1]
+    
+    
+    
+    
