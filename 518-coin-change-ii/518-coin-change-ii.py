bb73b8d9d9efe29572928053687b2196499b9cb0
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-10 16:54:56 +0100

    Time: 1066 ms (24.57%), Space: 13.8 MB (99.11%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,20 +1,35 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        dp = [[0]*(amount+1) for coin in range(len(coins)+1)]
+#         dp = [[0]*(amount+1) for coin in range(len(coins)+1)]
         
-        for c in range(len(coins)+1):
-            dp[c][0] = 1
+#         for c in range(len(coins)+1):
+#             dp[c][0] = 1
+        
+#         # r and c stand for row and column
+#         for r in range(1, len(coins)+1):
+#             for c in range(1, amount+1):
+#                 if c < coins[r-1]:
+#                     dp[r][c] = dp[r-1][c]
+#                 else:
+#                     dp[r][c] = dp[r-1][c] + dp[r][c-coins[r-1]]
+
+#         return dp[-1][-1]
+
+
+        dp = [0]*(amount+1)
+        
+        dp[0] = 1
         
         # r and c stand for row and column
         for r in range(1, len(coins)+1):
             for c in range(1, amount+1):
                 if c < coins[r-1]:
-                    dp[r][c] = dp[r-1][c]
+                    pass
                 else:
-                    dp[r][c] = dp[r-1][c] + dp[r][c-coins[r-1]]
+                    dp[c] += dp[c-coins[r-1]]
 
-        return dp[-1][-1]
+        return dp[-1]
                     
         # 2 options
         # => we use the curr coin +
