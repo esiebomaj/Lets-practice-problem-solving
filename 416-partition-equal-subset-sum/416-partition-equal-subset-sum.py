c870dd98df1db7927d743267abcda5727d0b27d6
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-11 17:10:19 +0100

    Time: 2459 ms (39.73%), Space: 148.6 MB (10.99%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -9,8 +9,28 @@ class Solution:
         # if we can make this target then we can return true
         target = sum(nums)//2
         
-        
         # using a brute force dfs method to check all the posible combinations
+        def dfs(i,currSum):
+            
+            if currSum == target:
+                return True
+            if currSum > target or i >= len(nums):
+                return False
+            
+            return dfs(i+1, currSum+nums[i]) or dfs(i+1, currSum)
+           
+        
+        return dfs(0, 0)
+    
+    def canPartition(self, nums: List[int]) -> bool:
+        # same idea as the first solution 
+        # but this time using memoization in our dfs to reduce repeated computations
+        
+        if sum(nums)%2 != 0:
+            return False
+        
+        target = sum(nums)//2
+        
         memo = {}
         def dfs(i,currSum):
             if (i,currSum) in memo:
