4061d776d84a96ca2b6c6a1d722b13d8df4eb668
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-11 18:39:44 +0100

    Time: 5911 ms (9.15%), Space: 29.6 MB (37.84%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -49,6 +49,59 @@ class Solution:
             return rt
         
         return dfs(0, 0)
-            
+    
+    def canPartition(self, nums: List[int]) -> bool:
+        # same idea as the first solution 
+        # but this time using memoization in our dfs to reduce repeated computations
+        
+        if sum(nums)%2 != 0:
+            return False
+        
+        target = sum(nums)//2
+        
+        # Now this simplifies to finding if we can make target from nums
+        # we can do this with a dp table 
+        
+        dp = [[False]*(target+1) for i in nums] 
+        
+        for i in range(target+1):
+            dp[0][i] = True if i == nums[0] else False
+        
+        for i in range(len(nums)):
+            dp[i][0] = True
+        
+        for r in range(1, len(nums)):
+            for c in range(1, target+1):
+                if c < nums[r]:
+                    dp[r][c] = dp[r-1][c]
+                else:
+                    dp[r][c] = dp[r-1][c] or dp[r-1][c-nums[r]]
+                    
+        # print(dp)
+        return dp[-1][-1]
+        
+        
+    #     [1,2,3,4] 5
+    
+    #     0,1,2,3,4,5
+    # 2   T F T F F F    (amount%coins[0])
+    # 3   T F T T F T
+    # 1   T T T T T T
+    # 4   T T T T T T
+        
+        
+       
+        
+        # first col is always F (cause we cannot make 0 using atleast 1 of n positve ints)  
+        
+        # if the amount is less than the current coin we take the up one
+        
+        # if the amount  is reater or equal to the current coin:
+        
+        # if the one above it is true then the one under is true too
+        # can we make the target with the coins before this coin? if yes the we can make it with this coin too
+        # SUMMMARY: dp[r][c] = dp[r-1][c] or dp[r-1][c-nums[r]]
+        
+        
         
         
