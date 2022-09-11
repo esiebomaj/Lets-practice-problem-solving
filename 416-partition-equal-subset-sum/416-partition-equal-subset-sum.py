9424ecd430aa402878e222e0f604a40c0104a3f6
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-11 17:06:51 +0100

    Time: 2873 ms (34.71%), Space: 148.6 MB (11.53%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # first thing thst comes to mind is to check that the sum divide by 2 is an whole number
+        # else we can just return false immidiately
+        if sum(nums)%2 != 0:
+            return False
+        
+        # the division by 2 becomes our target 
+        # if we can make this target then we can return true
+        target = sum(nums)//2
+        
+        
+        # using a brute force dfs method to check all the posible combinations
+        memo = {}
+        def dfs(i,currSum):
+            if (i,currSum) in memo:
+                return memo[(i,currSum)]
+            
+            if currSum == target:
+                return True
+            if currSum > target or i >= len(nums):
+                return False
+            
+            
+            rt = dfs(i+1, currSum+nums[i]) or dfs(i+1, currSum)
+            
+            memo[(i,currSum)] = rt
+            
+            return rt
+        
+        return dfs(0, 0)
+            
+        
+        
