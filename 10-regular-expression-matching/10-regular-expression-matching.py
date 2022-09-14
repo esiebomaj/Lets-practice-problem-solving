bb20ac6c0c160e5cf5f9aaa59711ef0c0da57389
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-14 09:06:54 +0100

    Time: 40 ms (98.17%), Space: 14.2 MB (15.00%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,8 +1,12 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
+        memo = {}
         
         def dfs(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+            
             if i >= len(s) and j >= len(p):
                 return True
             if j >= len(p):
@@ -11,8 +15,11 @@ class Solution:
             isMatch = (i < len(s) and j < len(p)) and (s[i] == p[j] or p[j] == ".")
             
             if j+1 < len(p) and p[j+1] == "*":
-                return dfs(i, j+2) or (isMatch and dfs(i+1, j))
+                rt = dfs(i, j+2) or (isMatch and dfs(i+1, j))
             else:
-                return isMatch and dfs(i+1, j+1)
+                rt = isMatch and dfs(i+1, j+1)
+            
+            memo[(i,j)] = rt
+            return rt
             
         return dfs(0,0)
