145499e425f21b98f8b1c282f2955186550a8d6e
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-18 19:54:57 +0100

    Time: 235 ms (17.51%), Space: 18.3 MB (24.84%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.py
@@ -1,31 +1,26 @@
 import heapq
-
 class KthLargest:
-    # we use a min head (priority q) of len k to keep track of the value
-    # after adding a value we ensure that the heap is of size k by using a while loop and heappop
-    # this way the heapq contains the k largest values
-    # and the k largest value is the smallest value in the heap i.e heap[0]
 
     def __init__(self, k: int, nums: List[int]):
-        print(nums)
         heapq.heapify(nums)
+        self.heap = nums
         self.k = k
-        while len(nums) > self.k:
-            heapq.heappop(nums)
         
-        self.nums = nums
-        print(nums)
-             
+        for i in range(len(nums)-k):
+            heapq.heappop(self.heap)
         
+        
+        
+
     def add(self, val: int) -> int:
-        heapq.heappush(self.nums, val)
-        while len(self.nums) > self.k:
-            heapq.heappop(self.nums)
-            
-        return self.nums[0]
+        heapq.heappush(self.heap, val)
+        for i in range(len(self.heap) - self.k):
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]
         
 
-
+# [4, 5, 8, 2] [2,4,4,5,5,8,9,10]
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
