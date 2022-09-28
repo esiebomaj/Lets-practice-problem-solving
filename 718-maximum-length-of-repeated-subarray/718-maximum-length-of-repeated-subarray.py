ab56def97aa4c6e88f86b953f87b941c8e450c55
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-09-28 03:16:08 +0100

    Time: 7882 ms (21.70%), Space: 40 MB (19.12%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,5 +1,29 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        # if there are unique values
+        # then we can solve ths problem using a hashmap as so
+#         hashmap = {}
+#         res = 0
+#         for i in range(len(nums1)):
+#             hashmap[nums1[i]] = i
+        
+#         i = 0
+#         while i < len(nums2):
+#             num = nums2[i]
+#             if num in hashmap:
+#                 j = i
+#                 k = hashmap[num]
+#                 currLongest = 0
+#                 while j < len(nums2) and k < len(nums1) and nums2[j] == nums1[k]:
+#                     currLongest += 1
+#                     j += 1
+#                     k += 1
+#                 res = max(currLongest, res)
+#                 i = j-1
+                
+#             i += 1
+#         return res
+                
         
         dp = [[0 for i in range(len(nums1)+1)] for k in range(len(nums2)+1)]
         res = 0
