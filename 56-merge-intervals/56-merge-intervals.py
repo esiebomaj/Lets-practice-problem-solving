c86a6c70742884bbc71a2a7866e108689341633c
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-01 10:19:06 +0100

    Time: 386 ms (9.28%), Space: 18.2 MB (53.32%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -7,19 +7,19 @@ class Solution:
         for i in range(1, len(intervals)):
             interval = intervals[i]
         
-            #if overlap
-            # we merge and continue 
+            
             if not merged[1] < interval[0] and not merged[0] > interval[1]:
+                # if overlap
+                # we merge and continue 
                 merged = [min(merged[0], interval[0]), max(merged[1], interval[1])]
+                
             else:
+                # if not overlap
+                # we append merge 
+                # reinitialize merge to interval 
                 res.append(merged)
                 merged = interval
                 
-            
-            # if not overlap
-            # we append merge 
-            # reinitialize merge to interval 
-            
             
         # append merge 
         res.append(merged)
