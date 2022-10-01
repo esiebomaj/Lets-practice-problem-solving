7408edf73cf330d73a64bf87719bfd7b6e0b47cf
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-01 08:04:09 +0100

    Time: 880 ms (36.18%), Space: 29.5 MB (39.10%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,42 +1,29 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0]*(amount+1) for i in range(len(coins)+1)]
+        for i in range(len(coins)+1):
+            dp[i][0] = 1
         
-##### More intuitive solution using m by n time and space
-##### Understand this soution forst before moving to the second one
-        
-#         dp = [[0]*(amount+1) for coin in range(len(coins)+1)]
-        
-#         for c in range(len(coins)+1):
-#             dp[c][0] = 1
-        
-#         # r and c stand for row and column
-#         for r in range(1, len(coins)+1):
-#             for c in range(1, amount+1):
-#                 if c < coins[r-1]:
-#                     dp[r][c] = dp[r-1][c]
-#                 else:
-#                     dp[r][c] = dp[r-1][c] + dp[r][c-coins[r-1]]
-
-#         return dp[-1][-1]
-
-
-
-##### Optimized soltion using M*N time but M space
-
-        dp = [0]*(amount+1)
-        
-        dp[0] = 1
-        
-        # r and c stand for row and column
-        
-        for r in range(1, len(coins)+1):
-            for c in range(1, amount+1):
-                if c < coins[r-1]:
-                    pass
+        for r in range(1,len(coins)+1):
+            for c in range(1,amount+1):
+                coin = coins[r-1]                        
+                if coin > c:
+                    dp[r][c] = dp[r-1][c]
                 else:
-                    dp[c] += dp[c-coins[r-1]]
-
-        return dp[-1]
+                    dp[r][c] = dp[r-1][c] + dp[r][c-coin]
+        
+        return dp[-1][-1]
+                
                     
-        # 2 options
-        # => we use the curr coin +  we dont use the curr coin        
+"""
+    0,1,2,3,4,5
+[]  1 0 0 0 0 0
+[1] 1 1 1 1 1 1
+[2] 1 1 2 2 3 3
+[5] 1 1 1 1 1 4
+
+use it + dont use it
+
+dont use it = dp[r-1][c]
+use it = dp[r][c-coin] 
+"""
