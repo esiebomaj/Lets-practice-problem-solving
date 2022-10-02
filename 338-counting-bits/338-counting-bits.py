e84ded1f12f58e6400714e12a1b650fef06cd28d
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-02 20:26:25 +0100

    Time: 171 ms (49.58%), Space: 20.9 MB (30.29%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+        for i in range(0,n+1):
+            result.append(bin(i)[2:].count('1'))
+            # result.append(self.toBinary(i).count('1'))
+
+        return result
+    
+    def toBinary(self, num):
+        if num == 0:
+            return '0'
+        
+        remainders = ''
+        while  num > 0:
+            re = num%2
+            remainders = str(re) + remainders
+            num = num//2
+            
+        return remainders
+            
+#         if num == 1:
+#             return '1'
+#         if num == 0:
+#             return ''
+        
+#         return self.toBinary(num//2) + f'{num%2}'
+        
