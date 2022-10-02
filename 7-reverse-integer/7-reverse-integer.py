b332b25f09216dd4707a7fc5182f3eb2e52edec8
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-02 19:28:09 +0100

    Time: 74 ms (9.32%), Space: 14 MB (16.49%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,18 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        signed = x < 0
+        negative = x < 0
         x = abs(x)
         y = 0
+        i = len(str(x))-1
         while x:
-            print(x)
-            y += (x%10)*(10**(len(str(x))-1))
+            y += (x%10)*(10**i)
             x = x//10
-        # print(y)
-        
-        # y = y % ((2**31)-1)
-        # print(y % ((2**31)-1))
-        # print(((2**32)))
-        if y >  (2**31):
+            i -= 1
+
+
+        if y > (2**31):
             return 0
         
-        return y if not signed else -y
+        return y if not negative else -y
