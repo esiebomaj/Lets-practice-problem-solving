4c5e1ecba9c553b02135aae9b6a9feb50cc7ced1
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-02 19:28:46 +0100

    Time: 88 ms (5.77%), Space: 13.7 MB (96.99%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -2,15 +2,16 @@ class Solution:
     def reverse(self, x: int) -> int:
         negative = x < 0
         x = abs(x)
-        y = 0
+        res = 0
         i = len(str(x))-1
+        
         while x:
-            y += (x%10)*(10**i)
+            res += (x%10)*(10**i)
             x = x//10
             i -= 1
 
 
-        if y > (2**31):
+        if res > (2**31):
             return 0
         
-        return y if not negative else -y
+        return res if not negative else -res
