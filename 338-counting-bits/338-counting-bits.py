05f9ea6cc57146e2796f6a4998299c5ddc3a4cf2
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-03 10:22:52 +0100

    Time: 206 ms (34.08%), Space: 20.8 MB (79.05%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,12 +1,21 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        dp = [0]*(n+1)
+        offset = 1
+        
+        for i in range(1, n+1):
+            offset = i if i == offset*2 else offset
+            dp[i] = 1 + dp[i-offset]
+            
+        return dp
+    
         result = []
-        for i in range(0,n+1):
+        for i in range(0,n+1): # NlogN
             result.append(self.toBinary(i).count('1'))
 
         return result
     
-    def toBinary(self, num):
+    def toBinary(self, num): # Log(n)
         if num == 0:
             return '0'
         
@@ -17,4 +26,3 @@ class Solution:
             num = num//2
             
         return remainders
-        
