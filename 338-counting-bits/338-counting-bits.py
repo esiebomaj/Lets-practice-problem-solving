56bdf5c9d072b61ee0d67abab1727dc316708dc6
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-03 10:46:52 +0100

    Time: 172 ms (49.55%), Space: 20.6 MB (97.08%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,5 +1,62 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        # O(N) solution 
+        """
+        We want to try to get a repeating pattern in the counts of 1
+        
+        Let use 10
+        0 = 0
+        1 = 1
+        2 = 10
+        3 = 11
+        
+        4 = 100
+        5 = 101
+        6 = 110
+        7 = 111
+        
+        8 =  1000
+        9 =  1001
+        10 = 1010
+        11 = 1011
+        12 = 1100
+        13 = 1101
+        14 = 1110
+        15 = 1111
+        
+        16 = 10000
+        17 = 10001
+        18 = 10010
+        19 = 10011
+        20 = 10100
+        
+        We can see that there is a pattern 
+        at [2,4,8,16,32... etc] the sequence repeats from 0 with an addition of an extra 1 most significant bit
+        i.e @ 4 the sequence starts from 0 with an aditional 1 as the MSB
+        
+        4 => 1 00
+        5 => 1 01
+        6 => 1 10
+        7 => 1 11
+        
+        The pattern start again at 8
+        repeating sequence from 0 with a extra 1 in the MSB
+        
+        8 =>  1 000
+        9 =>  1 001
+        10 => 1 010
+        11 => 1 011
+        12 => 1 100
+        13 => 1 101
+        14 => 1 110
+        15 => 1 111
+        
+        So we can conclude that 
+        dp[i] = 1 + dp[i-offset]
+        where offset is [2,4,8,16, etc]
+        so at any of this points we have to adjust our offset 
+        this makes it posible to start from 0 at any of these point
+        """
         dp = [0]*(n+1)
         offset = 1
         
@@ -8,6 +65,7 @@ class Solution:
             dp[i] = 1 + dp[i-offset]
             
         return dp
+    
     
         result = []
         for i in range(0,n+1): # NlogN
