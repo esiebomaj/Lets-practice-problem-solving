67162b38b998c199343866395ca213c566eb4e18
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-04 19:59:45 +0100

    Time: 70 ms (13.45%), Space: 13.9 MB (11.99%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,5 +1,30 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # Optimized solution
+        # TIme: O(N) space: O(1)
+        
+        digits = digits[::-1]
+        carry = 0
+        
+        for i in range(len(digits)):
+            digit = digits[i]
+            
+            if i == 0:
+                digit = digit + 1
+                
+            digit = digit + carry
+            
+            carry, newDigit = digit//10, digit%10
+            
+            digits[i] = newDigit
+            
+        if carry:
+            digits.append(carry)
+        
+        return digits[::-1]
+    
+                
+        # Time O(N), space: O(N)
         res = []
         carry = 0
         for i in range(len(digits)-1, -1, -1):
@@ -12,11 +37,10 @@ class Solution:
             carry, digit = digit//10, digit%10
             
             res.append(digit)
+            
+            
         if carry:
             res.append(carry)
+            
         return res[::-1]
-    
-    # [1,2,9]
-                
-
         
