0c4a5dcdb4c1452f7cbdf32349c2dbef0136dc77
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-05 09:31:26 +0100

    Time: 4456 ms (11.34%), Space: 57.4 MB (27.28%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -1,39 +1,26 @@
-
 import heapq
 
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        sizes = [((end-start)+1, start, end) for start, end in intervals]
-        
-        sizes.sort(key=lambda x:x[1])
-        res = {}
-        heap = []        
         i = 0
+        heap = []
+        intervals.sort()
+        res = {}
+        
         for q in sorted(queries):
-            while i < len(sizes) and sizes[i][1] <= q:
-                if sizes[i][1] <= q <= sizes[i][2]:
-                    heapq.heappush(heap, sizes[i])
-                i+=1
-                
-            while heap and heap[0][2] < q:
+            
+            while i < len(intervals) and q >= intervals[i][0]:
+                interval = intervals[i]
+                left = interval[0]
+                right = interval[1]
+                size = right - left + 1
+                heapq.heappush(heap, [size, left, right])
+                i += 1
+            
+            while heap and q > heap[0][2]:
                 heapq.heappop(heap)
-            
-            res[q] = heap[0][0] if heap else -1
-        
-        return [res[i] for i in queries]
-            
                 
-        
-        # print(sizes)
-#         res = [-1]*len(queries)
-        
-#         for i,query in enumerate(queries):
-#             for size, interval in sizes:
-#                 if interval[0] <= query <= interval[1]:
-#                     res[i]=size
-#                     break
-
-#         return res
+            # print(heap)  
+            res[q] = heap[0][0] if heap else -1
             
-            
-        
+        return [res[i] for i in queries]
