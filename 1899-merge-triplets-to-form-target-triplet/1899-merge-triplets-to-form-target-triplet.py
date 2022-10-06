5c67fe096e65b8d0bef7420ece70217ae9793482
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-06 22:37:30 +0100

    Time: 5739 ms (5.05%), Space: 59.7 MB (6.83%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        cantUse = set()
+        good = set()
+        for i,n in enumerate(target):
+            for j,t in enumerate(triplets):
+                if t[i] > n:
+                    cantUse.add(j)
+                    
+        if len(cantUse) >= len(triplets):
+            return False
+        
+        for i,n in enumerate(target):
+            for j,t in enumerate(triplets):
+                if t[i] == n and j not in cantUse:
+                    good.add(i)
+                    
+        return len(good) == 3 
+        
