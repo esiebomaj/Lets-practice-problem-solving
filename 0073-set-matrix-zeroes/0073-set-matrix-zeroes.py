d955b3100ce02c729f17b08853e1a4889147fe52
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-10 21:50:44 +0100

    Time: 263 ms (46.81%), Space: 14.7 MB (90.40%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,22 +3,52 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        
+        
         rows = len(matrix)
         cols = len(matrix[0])
         
-        zeroRows = set()
-        zeroCols = set()
+        firstRow = False
+        firstCol = False
         
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c] == 0:
-                    zeroCols.add(c)
-                    zeroRows.add(r)
-                    
-        for r in range(rows):
-            for c in range(cols):
-                if r in zeroRows or c in zeroCols:
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+                    if r == 0:
+                        firstRow = True
+                    if c == 0:
+                        firstCol = True
+                        
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
+        
+        if firstRow:
+            for c in range(cols):
+                matrix[0][c] = 0
+                
+        if firstCol:
+            for r in range(rows):
+                matrix[r][0] = 0
+                
+        
+        
+#         zeroRows = set()
+#         zeroCols = set()
+        
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if matrix[r][c] == 0:
+#                     zeroCols.add(c)
+#                     zeroRows.add(r)
+                    
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if r in zeroRows or c in zeroCols:
+#                     matrix[r][c] = 0
                     
         
         
