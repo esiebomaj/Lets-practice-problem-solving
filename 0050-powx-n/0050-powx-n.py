afea66310a72985c9ad69ba70b2c8774c04bf257
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-11 06:56:02 +0100

    Time: 68 ms (11.50%), Space: 13.8 MB (67.98%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,24 +5,38 @@ class Solution:
             return 0
         if n == 0:
             return 1
-        
-        res = x
+        res = 1
         negPow = n < 0
-       
+        
         n = abs(n)
         
-        j = 1
-        
-        # n = 10
-        # j = 8, res = 16*16
-        
-        while j*2 <= n:
-            res = res * res
-            j = j * 2
-            
-        
-        for i in range(j, n):
+        for i in range(n):
             res = res*x
         
         return 1/res if negPow else res
+
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         return x**n
+#         if x == 0:
+#             return 0
+#         if n == 0:
+#             return 1
+        
+#         res = x
+#         negPow = n < 0
+       
+#         n = abs(n)
+        
+#         j = 1
+        
+#         while j*2 <= n:
+#             res = res * res
+#             j = j * 2
+            
+        
+#         for i in range(j, n):
+#             res = res*x
+        
+#         return 1/res if negPow else res
         
