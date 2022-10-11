3fc243e7b34a62124143fb656c11c065fc01c110
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-11 06:13:50 +0100

    Time: 52 ms (54.89%), Space: 13.9 MB (82.96%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -6,16 +6,13 @@ class Solution:
         c1, c2 = 0, len(matrix[0])-1
         
         while r1 <= r2 and c1 <= c2:
-            # print(r1, r2, c1, c2)
             for c in range(c1, c2+1):
                 res.append(matrix[r1][c])
             r1+=1
             
             if r1 > r2 or c1 > c2:
                 break
-            
-            # print(res)
-            
+                        
             for r in range(r1, r2+1):
                 res.append(matrix[r][c2])
             c2 -= 1
@@ -34,7 +31,5 @@ class Solution:
                 res.append(matrix[r][c1])
             c1 += 1
             
-                    
-        # print(res)
         return res
             
