83845ffe320c3da99617ac834cfcc1a254b78abb
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-11 07:04:40 +0100

    Time: 61 ms (69.06%), Space: 13.9 MB (73.03%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        """
+        2 non-negative intergers
+        represented as strings
+        """
+        
+        return str(eval(num1+"*"+num2))
+        
