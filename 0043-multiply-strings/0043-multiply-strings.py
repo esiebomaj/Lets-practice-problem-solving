b13a71a4b38ae49430f7b475aa38ebcc45a911c8
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-12 23:51:11 +0100

    Time: 250 ms (32.09%), Space: 14.1 MB (8.19%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -4,6 +4,21 @@ class Solution:
         2 non-negative intergers
         represented as strings
         """
+        res = []
         
-        return str(eval(num1+"*"+num2))
-        
+        for j,n in enumerate(reversed(num2)):
+            carry = 0
+            for i, m in enumerate(reversed(num1)):
+                x = (int(n)*int(m)) + carry
+                carry, x = x//10, x%10
+                if i == 0:
+                    res.append(x*(10**j))
+                else:
+                    res[-1] = res[-1] + ((10**(i+j))*x)
+                    
+            if carry > 0:
+                res[-1] = res[-1] + ((10**(i+j+1))*carry)
+                
+            
+        print(res)
+        return str(sum(res))     
