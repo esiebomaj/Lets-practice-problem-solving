8791f409d094427ea84ff58f8c5d601116d6a9d5
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-15 10:46:38 +0100

    Time: 216 ms (51.52%), Space: 14.7 MB (36.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,79 +1,48 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-
-        dic = {}
-        for i in t:
-            if i in dic:
-                dic[i] += 1
-            else:
-                dic[i] = 1
-                
         l,r = 0,0
-        count = len(dic)
-        resCount = float(inf)
         res = ''
-        while r < len(s) :
-            
-            if count == 0:
-                if r-l < resCount:
-                    res = s[l:r]
-                    resCount = len(res)
-                    
-
-                # move l
-                if s[l] in dic:
-                    former = dic[s[l]]
-                    dic[s[l]] += 1
-                    curr = dic[s[l]]
-
-                    if former <= 0 and curr > 0:
-                        count += 1
-                    if curr <= 0 and former > 0:
-                        count -= 1
-                
-                l += 1
-                
+        resLen = float(inf)
+        
+        tWindow = {}
+        sWindow = {}
+        
+        for i in t:
+            if i in tWindow:
+                tWindow[i] += 1
             else:
-                # move r
-                if s[r] in dic:
-                    former = dic[s[r]]
-                    dic[s[r]] -= 1
-                    curr = dic[s[r]]
-
-                    if former <= 0 and curr > 0:
-                        count += 1
-                    if curr <= 0 and former > 0:
-                        count -= 1
-                        
+                tWindow[i] = 1
+                sWindow[i] = 0
+                
+        goodCount = len(tWindow)
+        
+        while r < len(s) or goodCount == 0:
+            # print(s[l:r], goodCount)
+            if goodCount == 0:
+                # print(s[l:r])
+                if r-l < resLen:
+                    res = s[l:r]
+                    resLen = r-l
+                    
+                # reduce the window size by moving l
+                if s[l] in tWindow:
+                    char = s[l]
+                    sWindow[char] -= 1
+                    if sWindow[char] == tWindow[char]-1:
+                        goodCount += 1
+                    
+                l += 1
+            else:
+                # increase the window size by moving r 
+                if s[r] in tWindow:
+                    char = s[r]
+                    sWindow[char] += 1
+                    if sWindow[char] == tWindow[char]:
+                        goodCount -= 1
                 r += 1
                 
-        if count == 0:    
-            if r-l < resCount:
-                res = s[l:r]
-                resCount = len(res)
-                
-        while l <= (len(s)-len(t)) and count == 0:
-            if count == 0:
-                if r-l < resCount:
-                    res = s[l:r]
-                    resCount = len(res)
-            
-                # move l
-            if s[l] in dic:
-                former = dic[s[l]]
-                dic[s[l]] += 1
-                curr = dic[s[l]]
-
-                if former <= 0 and curr > 0:
-                    count += 1
-                if curr <= 0 and former > 0:
-                    count -= 1
-                
-                
-            l += 1
-                
-            
         return res
             
+            
+            
+        
