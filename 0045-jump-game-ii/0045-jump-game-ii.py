313f2f317345d5bd4e6807ce92ef6e1bb799c608
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-15 17:13:11 +0100

    Time: 4705 ms (21.77%), Space: 15.8 MB (7.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,21 +1,24 @@
+from collections import deque
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        memo = {}
+        if len(nums) == 1:
+            return 0
+        q = deque([(0,0)])
+        seen = set()
+        seen.add(0)
         
-        def dfs(i):
+        while q:
+            id, lvl = q.pop()
+            seen.add(id)
+            for j in range(1, nums[id]+1):
+                i = id + j
+                if i not in seen:
+                    if i == len(nums)-1:
+                        return lvl+1
+                    seen.add(i)
+                    q.appendleft((i, lvl+1))
             
-            if i in memo:
-                return memo[i]
             
-            if i >= len(nums)-1:
-                return 0
-            
-            rt = float(inf)
-            for j in range(1, nums[i]+1):
-                rt = min(rt, dfs(i+j)+1)
-            
-            memo[(i)] = rt
-            return rt
         
-        return dfs(0)
         
