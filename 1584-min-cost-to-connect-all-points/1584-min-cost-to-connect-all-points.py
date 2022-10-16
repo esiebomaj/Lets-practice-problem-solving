45a1af393e99b9ca0106cc7c58e98da6b39d3562
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-16 13:59:19 +0100

    Time: 5461 ms (30.89%), Space: 118.4 MB (48.18%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -6,16 +6,6 @@ class Solution:
         def manhattan(p1, p2):
             return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
         
-#         memo = {}
-        
-#         def getDist(p1, p2):
-#             combined = tuple(sorted((str(p1[0]) + str(p1[1]), str(p2[0]) + str(p2[1]))))
-#             if combined not in memo:
-#                 memo[combined] = manhattan(p1,p2)
-                
-#             return memo[combined] 
-        
-        
         p1 = points[0]
         q = [(0, p1[0], p1[1])]
         seen = set()
