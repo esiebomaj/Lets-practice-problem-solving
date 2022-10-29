9f637f889a320b8ababc54fd09ec58e84f357ac0
Esieboma Jeremiah <esiebomaj@gmail.com>
2022-10-29 08:55:59 +0100

    Time: 98 ms (86.01%), Space: 15 MB (6.68%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,12 +1,42 @@
+from collections import deque
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        targets = collections.defaultdict(list)
-        for a, b in sorted(tickets)[::-1]:
-            targets[a] += b,
-        route = []
-        def visit(airport):
-            while targets[airport]:
-                visit(targets[airport].pop())
-            route.append(airport)
-        visit('JFK')
-        return route[::-1]
+        tickets.sort()
+        
+        adj = {s:[] for s,d in tickets}
+        
+        for src, dest in tickets:
+            adj[src].append(dest)
+        
+        print(adj)
+            
+        res = ["JFK"]
+            
+        def dfs(src):
+            if len(res) == len(tickets)+1:
+                return True
+            
+            if src not in adj:
+                return False
+            
+            tmp = adj[src].copy()
+            for i,dest in enumerate(tmp):
+                res.append(dest)
+                adj[src].pop(i)
+                
+                if dfs(dest):
+                    return True
+                
+                res.pop()
+                adj[src].insert(i,dest)
+            return False
+                
+        dfs("JFK")
+        return res
+    {
+        
+    }
+                
+                
+                
+            
